508eb7f80f24c23a0c6f844f9847a57a0d43bc0d
kodo <524580860@qq.com>
2015-10-18 18:18:24 +0800

    init

---
+++ js/spa.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kodo on 15/10/18.
+ */
+var spa = (function() {
+   var initModule = function($container) {
+       spa.shell.initModule($container)
+   }
+
+   return {
+       initModule : initModule
+   }
+})();
